82b2937061ad7ddce7ad0f9e0d9e81f26009a5ec
dev_me_nyong <80152716+dev-MeNyong@users.noreply.github.com>
2022-12-30 16:20:36 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#container").addClass("start");
     $("nav li").click(function(){
         $("#container").css("max-width","100%");
         var id =$(this).attr("data-rol");
@@ -9,5 +10,10 @@ $(document).ready(function() {
         $("#"+ id).addClass("this").prevAll().addClass("prev");
         //클릭한 메뉴와 매칭되는 id에 this 클래스를 지정하고 그 앞의 모든 section태그는 .prev 클래스 지정
         $("#"+ id).nextAll().addClass("next");
+    });
+    $(".logo_box").click(function(){
+        $("nav li").removeClass("on");
+        $(".content").removeClass("Prev this next");
+        $("#container").css("max-width","1200px");
     });
 });
