dd0696ebc2c31b1eb095f21c1a8a630ed0fda43c
dev_me_nyong <80152716+dev-MeNyong@users.noreply.github.com>
2023-01-02 19:17:08 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -16,4 +16,27 @@ $(document).ready(function() {
         $(".content").removeClass("Prev this next");
         $("#container").css("max-width","1200px");
     });
+
+    $(".roll_left").click(function(){
+        $(".book_roll li").eq(0).insertAfter(".book_roll li:last-child");
+    });
+
+    $(".roll_right").click(function(){
+        $(".book_roll li").eq(-1).insertBefore(".book_roll li:first-child");
+    });
+
+    $(".book_roll li").click(function(){
+        var _this =$(this);
+        var liurl =_this.data("url");
+        $(".notebook").html();
+        $.ajax({
+            type :'post',
+            url : liurl,
+            dataType :'html',
+            success: function(data){
+                $(".notebook").html(data);
+            }
+        });
+    });
+
 });
